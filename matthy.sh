6349cc7ab727d9581ea2ef79424b7ca8e19e4b1f
matthypraise <ihwemathias@gmail.com>
2024-08-06 11:09:59 +0100

    this is my first assignment

---
+++ matthy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+	read -p "rnter your name:" name
+	read -p "enter your age:" age
+	read -p "enter your gender:" gender
+	read -p "enter your height:" height
+	read -p "enter your state of origin:" state of origin
+	read -p "enter youe lga:" lga
+	read -p "enter your secondary school:" secondary school
+	read -p "enter your primary school:" primary school
+	echo $name 
+	echo $age
+	echo $gender
+	echo $height
+	echo $state of origin
+	echo $lga
+	echo $secondary school
+	echo $primary school
+
+
